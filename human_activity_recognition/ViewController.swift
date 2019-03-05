739fd3d39ec0745ec20ac1836519e3a584ae40e1
mzu <mzu@zurich.ibm.com>
2019-03-05 18:28:31 +0100

    Initial Commit

---
+++ human_activity_recognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  human_activity_recognition
+//
+//  Created by Mario Zusag   on 05.03.19.
+//  Copyright © 2019 Mario Zusag  . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
